07e3879d251f28166f221290b9182084b5075f4f
ObjectsCountries <oandcofficial@gmail.com>
2024-07-25 00:06:30 -0700

    Switch from iced to egui

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,73 @@
-use iced::{
-    alignment,
-    widget::{container, text},
-    Element, Length, Sandbox, Settings,
-};
-
-extern crate iced;
-
-#[derive(Debug, Clone)]
-enum Feature {
+extern crate eframe;
+extern crate egui;
+#[derive(Debug, Clone, Default, PartialEq)]
+enum Segment {
+    #[default]
+    Empty,
+    User,
+    UserDevice,
     Battery,
     Network,
     Time(String),
     Custom(String),
 }
 
-#[derive(Debug, Clone)]
-enum Message {
-    Add(Feature),
-    Remove(usize),
-    Export(Vec<Feature>),
-}
-
+#[derive(Default)]
 struct PlaceSegmentsOn {
-    features: Vec<Feature>,
+    new_segment: Segment,
+    segments: Vec<Segment>,
+    strftime: String,
+    custom_input: String,
 }
 
-impl Sandbox for PlaceSegmentsOn {
-    type Message = Message;
-
-    fn new() -> PlaceSegmentsOn {
-        Self { features: vec![] }
-    }
-
-    fn title(&self) -> String {
-        String::from("Place Segments On")
-    }
-
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            Message::Add(feature) => {
-                self.features.push(feature);
-            }
-            Message::Remove(position) => {
-                self.features.remove(position);
-            }
-            Message::Export(feature_list) => {
-                export(feature_list);
-            }
-        };
-    }
-
-    fn view(&self) -> Element<Self::Message> {
-        container(text("Place Segments On"))
-            .height(Length::Fill)
-            .width(Length::Fill)
-            .align_x(alignment::Horizontal::Center)
-            .align_y(alignment::Vertical::Center)
-            .into()
-    }
-
-    fn theme(&self) -> iced::Theme {
-        iced::Theme::Dark
+impl PlaceSegmentsOn {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
     }
 }
 
-fn export(feature_list: Vec<Feature>) -> String {
-    String::from("")
-    //TODO: convert features to string
+impl eframe::App for PlaceSegmentsOn {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.text_edit_singleline(&mut self.strftime);
+            ui.text_edit_singleline(&mut self.custom_input);
+            egui::ComboBox::from_label("")
+                .selected_text(format!("{:?}", self.new_segment))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut self.new_segment, Segment::User, "User");
+                    ui.selectable_value(
+                        &mut self.new_segment,
+                        Segment::UserDevice,
+                        "User & Device",
+                    );
+                    ui.selectable_value(&mut self.new_segment, Segment::Empty, "Empty");
+                    ui.selectable_value(&mut self.new_segment, Segment::Battery, "Battery");
+                    ui.selectable_value(&mut self.new_segment, Segment::Network, "Network");
+                    ui.selectable_value(
+                        &mut self.new_segment,
+                        Segment::Time(self.strftime.clone()),
+                        "STRF Time",
+                    );
+                    ui.selectable_value(
+                        &mut self.new_segment,
+                        Segment::Custom(self.custom_input.clone()),
+                        "Custom",
+                    );
+                });
+            if ui.button("Add").clicked() {
+                self.segments.push(self.new_segment.clone());
+            }
+            ui.label(format!("{:?}", self.segments));
+        });
+    }
 }
 
 fn main() {
-    PlaceSegmentsOn::run(Settings::default()).unwrap();
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Place Segments On",
+        native_options,
+        Box::new(|cc| Ok(Box::new(PlaceSegmentsOn::new(cc)))),
+    )
+    .unwrap();
 }
